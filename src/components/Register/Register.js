b8211d89bd4f49c382a1cc106a8619fdf01ebb56
Shanay Murdock <murdock.shanay@gmail.com>
2020-03-26 16:42:58 -0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://afternoon-peak-50158.herokuapp.com/register', {
+    fetch('https://murmuring-plateau-48933.herokuapp.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
